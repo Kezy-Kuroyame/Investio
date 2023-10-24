b0bccebfaae45268ab5203717bfa503381d1737a
Valerie <valeria_serebrennikova@mail.ru>
2023-10-25 00:01:35 +0300

    Creating HTTP API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,9 +45,17 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("org.postgresql:postgresql:42.2.6")
+    implementation("org.postgresql:postgresql:42.3.1")
 
-    implementation("com.squareup.retrofit2:converter-gson:2.5.0")
-    implementation("com.google.code.gson:gson:2.6.2")
+    implementation("io.ktor:ktor-server-core-jvm:2.3.5")
+    implementation("io.ktor:ktor-server-netty-jvm:2.3.5")
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.5")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.5")
+    implementation("io.ktor:ktor-server-status-pages-jvm:2.3.5")
+    implementation("io.ktor:ktor-server-default-headers-jvm:2.3.5")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+
+
 
 }
