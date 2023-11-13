66b7bb05ae4f13fc60ef9a348dea43adc88c600f
Kezy-Kuroyame <Kezy2807@gmail.com>
2023-11-13 20:48:58 +0300

    Compose interface !!!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.empty_views_activity"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.empty_views_activity"
@@ -33,9 +33,21 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.2"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/*"
+        }
+    }
 }
 
 dependencies {
+    implementation("androidx.navigation:navigation-compose:2.7.5")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -56,6 +68,18 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
 
-
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("androidx.compose.material:material-icons-extended")
 
 }
--- app/src/main/java/com/example/empty_views_activity/MainActivity.kt
+++ app/src/main/java/com/example/empty_views_activity/MainActivity.kt
@@ -9,42 +9,53 @@ import android.widget.EditText
 import android.widget.ListView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+import com.example.empty_views_activity.app.InvestioApp
+import com.example.empty_views_activity.navigation.Navigation
 import for_sql.ConnectionDB
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
 
-        val buttonLoginIn = findViewById<Button>(R.id.buttonLoginIn)
-        val buttonRegister = findViewById<Button>(R.id.buttonRegister)
-        val loginEmailAddress = findViewById<EditText>(R.id.loginEmailAddress)
-
-        val probe = findViewById<TextView>(R.id.probe)
-
-        val connectionDB = ConnectionDB()
-
-        if (connectionDB.isDatabaseConnected()) {
-            probe.text = "Соединение с базой данных установлено"
-        } else {
-            probe.text = "Ошибка при установлении соединения с базой данных"
+            Navigation()
         }
-
-        buttonLoginIn.setOnClickListener(){ loginIn(loginEmailAddress) }
-        buttonRegister.setOnClickListener(){ register() }
-
+//
+//        val buttonLoginIn = findViewById<Button>(R.id.buttonLoginIn)
+//        val buttonRegister = findViewById<Button>(R.id.buttonRegister)
+//        val loginEmailAddress = findViewById<EditText>(R.id.loginEmailAddress)
+//
+//        val probe = findViewById<TextView>(R.id.probe)
+//
+//        val connectionDB = ConnectionDB()
+//
+//        if (connectionDB.isDatabaseConnected()) {
+//            probe.text = "Соединение с базой данных установлено"
+//        } else {
+//            probe.text = "Ошибка при установлении соединения с базой данных"
+//        }
+//
+//        buttonLoginIn.setOnClickListener(){ loginIn(loginEmailAddress) }
+//        buttonRegister.setOnClickListener(){ register() }
+//
+//    }
+//
+//    fun loginIn(loginEmailAddress: EditText){
+//        val intent = Intent(this, ActivityPasswordLogging::class.java)
+//
+//        intent.putExtra("mail", loginEmailAddress.text.toString())
+//        startActivity(intent)
+//    }
+//
+//    fun register(){
+//        val intent = Intent(this, RegistrationActivity::class.java)
+//        startActivity(intent)
+//    }
     }
-
-    fun loginIn(loginEmailAddress: EditText){
-        val intent = Intent(this, ActivityPasswordLogging::class.java)
-
-        intent.putExtra("mail", loginEmailAddress.text.toString())
-        startActivity(intent)
-    }
-
-    fun register(){
-        val intent = Intent(this, RegistrationActivity::class.java)
-        startActivity(intent)
-    }
-
 }
+
+
